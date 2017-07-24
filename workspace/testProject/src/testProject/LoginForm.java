531846afbb25ffe61e6b8ae265c95be143133ff6
hwanghyohyuk <hhhwang618@naver.com>
2017-07-24 22:46:49 +0900

    170724

--- workspace/testProject/src/testProject/LoginForm.java
+++ workspace/testProject/src/testProject/LoginForm.java
@@ -10,6 +10,7 @@ public class LoginForm extends JFrame {
 	public LoginForm() {
 		setResizable(false);
 		this.setSize(800,600);
+		this.setLocationRelativeTo(null);
 		
 		setIconImage(Toolkit.getDefaultToolkit().getImage("D:\\KH_Backup\\KHGIT\\workspace\\testProject\\img\\galgeyo.png"));
 		setTitle("갈게요");
@@ -20,43 +21,44 @@ public class LoginForm extends JFrame {
 		lblNewLabel.setBackground(Color.WHITE);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setIcon(new ImageIcon("D:\\KH_Backup\\KHGIT\\workspace\\testProject\\img\\logo3.png"));
-		lblNewLabel.setBounds(12, 10, 497, 552);
+		lblNewLabel.setBounds(12, 10, 532, 552);
 		getContentPane().add(lblNewLabel);
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.WHITE);
-		panel.setBounds(521, 0, 273, 572);
+		panel.setBounds(556, 0, 238, 572);
 		getContentPane().add(panel);
 		panel.setLayout(null);
 		
 		JButton btnLogin = new JButton("로그인");
-		btnLogin.setBounds(12, 346, 249, 42);
+		btnLogin.setBounds(8, 357, 218, 42);
 		panel.add(btnLogin);
 		btnLogin.setBackground(Color.WHITE);
 		
 		textFieldID = new JTextField();
-		textFieldID.setBounds(12, 216, 249, 30);
+		textFieldID.setBounds(8, 227, 218, 30);
 		panel.add(textFieldID);
 		textFieldID.setColumns(10);
 		
 		textFieldPassword = new JTextField();
-		textFieldPassword.setBounds(12, 256, 249, 30);
+		textFieldPassword.setBounds(8, 267, 218, 30);
 		panel.add(textFieldPassword);
 		textFieldPassword.setColumns(10);
 		
 		JCheckBox checkBox = new JCheckBox("로그인 유지");
-		checkBox.setBounds(12, 292, 100, 20);
+		checkBox.setBounds(8, 303, 100, 20);
 		panel.add(checkBox);
 		checkBox.setBackground(Color.WHITE);
 		
 		JLabel lblRegistration = new JLabel("회원가입");
-		lblRegistration.setBounds(12, 542, 60, 20);
+		lblRegistration.setBackground(Color.WHITE);
+		lblRegistration.setBounds(8, 542, 60, 20);
 		panel.add(lblRegistration);
 		lblRegistration.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		JLabel lblFindIdPass = new JLabel("아이디 / 패스워드 찾기");
-		lblFindIdPass.setBounds(121, 542, 140, 20);
+		lblFindIdPass.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblFindIdPass.setBounds(86, 542, 140, 20);
 		panel.add(lblFindIdPass);
-		lblFindIdPass.setHorizontalAlignment(SwingConstants.CENTER);
 	}
 }
--- workspace/testProject/src/testProject/RegistrationForm.java
+++ workspace/testProject/src/testProject/RegistrationForm.java
@@ -4,6 +4,9 @@ import java.awt.*;
 
 public class RegistrationForm extends JFrame {
 	public RegistrationForm() {
+		setResizable(false);
+		this.setSize(800,600);
+		this.setLocationRelativeTo(null);
 	}
 
 
